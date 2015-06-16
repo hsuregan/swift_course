28a6e1f7afda5e5246678e9c08e5b983b2276279
hsuregan <hsuregan@gmail.com>
2015-06-16 01:07:34 -0500

    shape and circle

---
+++ Shape.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class Shape {
+    var color:UIColor
+    init(color:UIColor) {
+        self.color = color
+    }
+}
+
+class Circle: Shape {
+    var radius:Int
+    init(radius:Int, color:UIColor) {
+        self.radius = radius
+        super.init(color: color)
+    }
+    
+}
+
+var circle:Circle = Circle(radius: 5, color: UIColor.redColor());
+
+//circle.color;
