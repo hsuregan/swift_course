dc99e2fdc5b3f568795b656355fbcf5a54f3783a
hsuregan <hsuregan@gmail.com>
2015-07-05 13:34:40 -0400

    test gui

---
+++ RGB Sliders/RGB Sliders/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RGB Sliders
+//
+//  Created by Regan Hsu on 7/5/15.
+//  Copyright (c) 2015 Regan Hsu. All rights reserved.
+//
+//commit gui xcode
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
