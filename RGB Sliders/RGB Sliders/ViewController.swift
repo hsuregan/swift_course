13a8f014345cf576093a379e165d368e5dfb514b
hsuregan <hsuregan@gmail.com>
2015-07-06 13:13:03 -0400

    color gradient changes

--- RGB Sliders/RGB Sliders/ViewController.swift
+++ RGB Sliders/RGB Sliders/ViewController.swift
@@ -5,16 +5,18 @@
 //  Created by Regan Hsu on 7/5/15.
 //  Copyright (c) 2015 Regan Hsu. All rights reserved.
 //
-//ugh
-//ppooop
 
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateBackgroundColor()
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +24,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //arguments don't matter because we can refer to the redSlider, greenSlider, and blueSlider directly
+    @IBAction func updateBackgroundColor() {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
 
 }
 
