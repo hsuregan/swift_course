819e082a23a7ec7c94a90a831cf5661c9abca767
hsuregan <hsuregan@gmail.com>
2015-07-16 18:46:35 -0400

    glyphicon directory added and tableView tester

---
+++ Table View/Table View/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Table View
+//
+//  Created by Regan Hsu on 7/16/15.
+//  Copyright (c) 2015 Regan Hsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
